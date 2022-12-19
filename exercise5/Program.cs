3c97adb53e5a09f2a11f65576d64c38604efc776
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-12-19 16:47:32 +0700

    Add project files.

---
+++ exercise5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise5
+{
+    class Program
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+        public Program()
+        {
+            /*Intializing the values of the variables REAR and FRONT to -1 to indicate 
+           * the queue is intially empty. */
+            FRONT = -1;
+            REAR = -1;
+        }
+    }
+}
