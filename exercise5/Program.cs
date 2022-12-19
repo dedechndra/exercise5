dcb7abb90e0d207d1a89940cfbc3744a2d430522
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-12-19 16:54:06 +0700

    create method public void for remove

--- exercise5/Program.cs
+++ exercise5/Program.cs
@@ -8,6 +8,7 @@ namespace exercise5
 {
     class Program
     {
+
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
 
@@ -18,20 +19,61 @@ namespace exercise5
             FRONT = -1;
             REAR = -1;
         }
-    }
-    public void insert(int element)
-    {
-        // This statement checks for the overflow condition.
-        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        public void insert(int element)
         {
-            Console.WriteLine("\nQueue overflow\n");
-            return;
+            // This statement checks for the overflow condition.
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            /*this following statement checks whether the queue is empty. if the queue is empty
+             * , then the value of the  RWAR and FRONT variables is set to 0 */
+            if ((FRONT == -1))
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* if REAR is at the last position of the array, than the value of 
+                 * REAR is set to 0 that corresponds to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /*If REAR is not at the last position , then its values is increamented by one*/
+                    REAR = REAR + 1;
+            }
+            /* Once the position of REAR is determind, the element is added at its proper place. */
+            queue_array[REAR] = element;
         }
-        /*this following statement checks whether the queue is empty. if the queue is empty
-         * , then the value of the  RWAR and FRONT variables is set to 0 */
-        if ((FRONT == -1))
+        public void remove()
         {
-            FRONT = 0;
-            REAR = 0;
+            // checks wheather the queue is empty
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue underflow\n");
+                return;
+            }
+            Console.WriteLine("\nThe Element deleted from queue is: " + queue_array[FRONT]
+                + "\n");
+            // Check if the queue has one element. 
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                /* If the element to be deleted is at the last position of the array, then the value 
+                 * of FRONT is set to 0 i.e to the first element of the array. */
+                if (FRONT == max - 1)
+                    FRONT = 0;
+                else
+                    // FRONT is incremented by one if it is not the first element of array.
+                    FRONT = FRONT + 1;
+            }
         }
     }
+}
+
